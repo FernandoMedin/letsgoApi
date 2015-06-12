29fd660a83b8090770c55b7e5d88252c2333351d
Cesar Azevedo <cesardeazevedo@outlook.com>
2015-06-12 02:51:08 -0300

    added current user route

--- app/views.py
+++ app/views.py
@@ -1,11 +1,30 @@
 import datetime
+import json
 from app.token import TokenAuth
+from rest_framework.views import APIView
 from rest_framework import viewsets, permissions
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import detail_route
 from rest_framework.authentication import SessionAuthentication
 from app.serializers import ProfileSerializer, UserSerializer, OrgSerializer
 from app.models import User, Profile, Organizations, Event_Category, Event_Type, Events
 from app.serializers import Events_Serializer, Event_Category_Serializer, Event_Type_Serializer
+
+class Current(APIView):
+
+    authentication_classes = (TokenAuth,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request):
+
+        data = {
+            'email': request.user.email,
+            'first_name': request.user.first_name,
+            'last_name': request.user.last_name,
+        }
+
+        return Response(data)
 
 class Profile_ViewSet(viewsets.ModelViewSet):
 
